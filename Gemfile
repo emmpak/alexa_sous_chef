14dd2169187004445cb983b2f5aea8ea5a93249b
emmpak <eiliev05@yahoo.com>
2017-07-24 13:09:35 +0100

    Add alexa_verifier

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem "sinatra"
 gem 'fatsecret-api'
 gem 'pry'
+gem 'alexa_verifier'
 
 group :development, :test do
   gem 'dotenv'
--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'json'
+require 'alexa_verifier'
 require './lib/alexa/skill'
 require './lib/alexa/request'
 require './lib/alexa/response'
@@ -9,6 +10,13 @@ class AlexaChef < Sinatra::Base
   FatSecret.init(ENV["FATSECRET_KEY"],ENV["FATSECRET_SECRET"])
 
   post '/' do
+    verification_success = AlexaVerifier.new.verify!(
+    request.headers["HTTP_SIGNATURECERTCHAINURL"],
+    request.headers['HTTP_SIGNATURE'],
+    request.body.read
+    )
+    raise "Cert validation failed" unless verification_success
+
     Alexa::Handlers.handle(request)
   end
 end
